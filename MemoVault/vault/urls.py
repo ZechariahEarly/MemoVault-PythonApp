2005cc215c6a3c06f7d02ace44b0a53e927348c8
Zechariah Early <zech@Zechariahs-MacBook-Pro.local>
2024-06-13 10:29:15 -0500

    Styling updates and added scrollable

--- MemoVault/vault/urls.py
+++ MemoVault/vault/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
 from . import views
+from .views import VaultView
 
 app_name = 'vault'
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", VaultView.index, name="vault")
 ]
--- MemoVault/vault/views.py
+++ MemoVault/vault/views.py
@@ -3,17 +3,30 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from .models import Document
+
 from .forms import DocumentForm
 
-@login_required
-def index(request):
-    if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
-            profile = form.save(commit=False)
-            profile.user = request.user
-            profile.save()
-        else:
-            print(form.errors)
-    context = {'form': DocumentForm()}
-    return render(request, "vault/index.html", context)
+
+class VaultView(View):
+    @login_required
+    def index(request):
+        if request.method == 'POST':
+            form = DocumentForm(request.POST, request.FILES)
+            if form.is_valid():
+                profile = form.save(commit=False)
+                profile.user = request.user
+                profile.save()
+            else:
+                print(form.errors)
+            context = {'form': DocumentForm(), 'files': VaultView.get_vault_files(request)}
+        if request.method == 'GET':
+            context = {'form': DocumentForm(), 'files': VaultView.get_vault_files(request)}
+        return render(request, "vault/vault.html", context)
+
+
+    def get_vault_files(request):
+        return (Document.objects.filter(user=request.user)
+                .filter(file__isnull=False)
+                )
